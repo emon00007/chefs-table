03f4470a06efa4c9f6e8f6f5dca1e33e90581b09
emon00007 <skemon152@gmail.com>
2024-03-16 23:55:42 +0600

    side bar section complete

--- src/assets/Component/Addtocut/Addtocut.jsx
+++ src/assets/Component/Addtocut/Addtocut.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
-
-const Addtocut = (addtocat) => {
-    const { recipe_name,calories,preparing_time } =addtocat;
+const Addtocut = ({addtocat,index}) => {
+    
+    const {recipe_name,preparing_time,calories}=addtocat
+    console.log(recipe_name)
     return (
         <div>
-            
+            <div class="overflow-x-auto">
+  <table class="table">
+   
+    <tbody>
+      <tr class="hover">
+        <th>{index+1}</th>
+        <td>{recipe_name}</td>
+        <td>{preparing_time}</td>
+        <td>{calories}</td>
+        <td><button className="btn bg-[#0BE58A] rounded-3xl text-black">Preparing</button></td>
+      </tr>
+      
+    </tbody>
+  </table>
+</div>
         </div>
     );
 };
--- src/assets/Component/SideBar/SideBar.jsx
+++ src/assets/Component/SideBar/SideBar.jsx
@@ -1,43 +1,37 @@
 import PropTypes from 'prop-types';
-const SideBar = ({addtocat}) => {
-    
-    //    console.log(recipe_name)
-    console.log(addtocat)
+import Addtocut from '../Addtocut/Addtocut';
+
+const SideBar = ({ addtocat }) => {
+
     return (
         <div className="1/3 text-center">
 
-       
             <div className="overflow-x-auto">
-            <h1 className="text-3xl my-4">Want to cook:{addtocat.length} </h1>
-                <table className="table">
-                    {/* <!-- head --> */}
-                    <thead>
-                        <tr>
-                            <th></th>
-                            <th>Name</th>
-                            <th>Time</th>
-                            <th>Calories</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-
-                        {/* <!-- row 2 --> */}
-                        <tr className="hover">
-                            <th>*</th>
-                            <td>{recipe_name}</td>
-                            <td>{preparing_time}</td>
-                            <td>{calories}</td>
-                            <button className="bg-[#0BE58A]">Preparing</button>
-                        </tr>
-
-                    </tbody>
-                </table>
+                <h1 className="text-3xl my-4 ">Want to cook:{addtocat.length} </h1>
+                <thead>
+                    <tr className='flex gap-16'>
+                        <th></th>
+                        <th>Name</th>
+                        <th>Time</th>
+                        <th>Calories</th>
+                    </tr>
+                </thead>
+                {
+                    addtocat.map((addtocat,index)=>
+                    <Addtocut key={addtocat.id}
+                    addtocat={addtocat} index={index} ></Addtocut>
+                    )
+                }
             </div>
         </div>
     );
 };
-// SideBar.propTypes = {
-//     addtocat: PropTypes.
-// }
+SideBar.propTypes = {
+    addtocat: PropTypes.object
+}
+
+
+
+
 
 export default SideBar;
